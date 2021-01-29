9cc4b76fe80137e53d0fac166bdec5b4cfc5cdb3
Roel Verbunt <roelv@fastmail.com>
2021-01-29 20:38:10 +0100

    Initial project

---
+++ src/main/java/nl/roelv/petclinic/PetclinicApplication.java
@@ -0,0 +1,13 @@
+package nl.roelv.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicApplication.class, args);
+	}
+
+}
