6e82dbd2978a74d56462b7e705b383b56d245e0f
Roel Verbunt <roelv@fastmail.com>
2021-02-06 17:22:52 +0100

    Add Vets index page + controller. Closes #7.

---
+++ petclinic-web/src/main/java/nl/roelv/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package nl.roelv.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping({"","/","/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
